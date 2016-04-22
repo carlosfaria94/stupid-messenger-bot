e79bf1f47ce85b35c3619528f83c86c748b49217
CarlosFaria94 <carlosfaria94@gmail.com>
2016-04-22 22:13:24 +0100

    - send hello - send echo

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var body = require('body-parser');
+var request = require('request');
+var token = process.env.VALIDATION_TOKEN;
 
 var app = express();
 
@@ -15,7 +17,7 @@ app.get('/ping', function(req, res) {
  */
 app.get('/webhook/', function (req, res) {
   // Setup your token as a environment variable
-  if (req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
+  if (req.query['hub.verify_token'] === token) {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong validation token');
@@ -32,10 +34,36 @@ app.post('/webhook/', function (req, res) {
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      console.log(text);
+      console.log(sender + " : " + text);
+      if (text ===  'hi' || 'Hi' || 'Hello' || 'Hi!') {
+        sendTextMessage(sender, "Hi! Nice to meet you! I'm a stupid boot!");
+      } else {
+        sendTextMessage(sender, "I would like to " + text.substring(0, 200));
+      }
     }
   }
   res.sendStatus(200);
 });
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.nerror) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(process.env.PORT || 3000);
